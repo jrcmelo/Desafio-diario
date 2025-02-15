f17c8192f3415242f5108ca67ea73d8a3bc7d0aa
John Robert <john.robert.c.melo@gmail.com>
2025-02-15 03:12:52 -0300

    Desafio 01 Concluido

---
+++ Desafio 01.py
@@ -0,0 +1,13 @@
+'''
+Quero que você faça uma função que receba um número de 1 a 26 e me retorne um Array com as letras do alfabeto, então por exemplo eu escolhi 2
+então eu quero que ele retorne um Array com a letra b.
+'''
+
+numero = int(input("Diga um numero de 1 a 26: "))
+alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
+if numero < 27 and numero > 0:
+    print(alfabeto[numero-1])
+else:
+    print("Digite um numero valido!")
