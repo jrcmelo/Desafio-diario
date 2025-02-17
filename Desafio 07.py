a23dc0a1bd4c7bd847e90636a8060bc582518864
John Robert <john.robert.c.melo@gmail.com>
2025-02-17 12:00:00 -0300

    Desafio 07 Concluido

--- Desafio 07.py
+++ Desafio 07.py
@@ -1,3 +1,30 @@
 '''
 Faça uma função que pegue duas matrizes e tenha como resultado a soma delas. Por exemplo: duas matrizes 2x2, as duas tenho 2, 2, 2, 2, o resultado da função será 4, 4, 4, 4
 '''
+matriz1 = [
+    [2, 2],
+    [2, 2]
+]
+
+matriz2 = [
+    [2, 2],
+    [2, 5]
+]
+
+resultado = [
+    [0, 0],
+    [0, 0]
+]
+
+
+def soma_matriz():
+    for i in range(len(matriz1)):
+        for j in range(len(matriz1[i])):
+            resultado[i][j] = matriz1[i][j] + matriz2[i][j]
+
+# Executando a soma
+soma_matriz()
+
+# Exibindo o resultado após a soma
+for linha in resultado:
+    print(linha)
