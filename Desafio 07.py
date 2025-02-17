5ff93770e392a92306d7fdb3dc590092a74de38a
John Robert <john.robert.c.melo@gmail.com>
2025-02-17 12:00:00 -0300

    Desafio 07 criado

---
+++ Desafio 07.py
@@ -0,0 +1,3 @@
+'''
+Faça uma função que pegue duas matrizes e tenha como resultado a soma delas. Por exemplo: duas matrizes 2x2, as duas tenho 2, 2, 2, 2, o resultado da função será 4, 4, 4, 4
+'''
